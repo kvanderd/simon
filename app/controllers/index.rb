cad9c51b04474f1c19bcd7f434ecdfe9db2ea84a
Katharine VanderDrift <me@katharine.org>
2013-06-18 16:16:52 -0700

    Update index.rb

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,12 +1,16 @@
+require "json"
 get '/' do
   # Look in app/views/index.erb
+  puts "i am in get"
   erb :index
 end
 
 post '/color' do
-
+  content_type :json
   #Create and return a JSON object with the random cell and color given below.
-
+  puts "I am in post"
   cell= rand(1..9)
   color= "#" + "%06x" % (rand * 0xffffff)
+ {cell: cell, color: color}.to_json
+ 
 end
