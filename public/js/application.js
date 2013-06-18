f360b1b9cd1a6cf40d90b35b1e080c6620ca26a1
Katharine VanderDrift <me@katharine.org>
2013-06-18 17:16:03 -0600

    Update application.js

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,15 @@
-$(document).ready(function(){
+$(document).ready(function() {
+  $('#get_color').on('click', function(e){
+		e.preventDefault();
+		$.ajax({
+			url:  '/color',
+			type: 'POST',
+			dataType: "json"
+		}).done(function(response){
+			console.log(response);
+			$('color_me, li:nth-child(' + response.cell +')').css('background-color', response.color)
+	  });
+  });
+}); 
 
-});
+
